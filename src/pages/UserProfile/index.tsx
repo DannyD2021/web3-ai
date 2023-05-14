b2b7185c896853ff11248de97d43be29bce45f01
sanshi <sanshi@sanshideMBP.lan>
2023-05-14 12:32:52 +0800

    feat: add my profile

--- src/pages/UserProfile/index.tsx
+++ src/pages/UserProfile/index.tsx
@@ -1,11 +1,30 @@
 import * as React from 'react';
 import styled from '@emotion/styled';
+import { useAccount } from 'wagmi';
+import { formatAddress } from '@/utils';
+
+const UserProfileContainer = styled.div`
+  .header {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    margin: 16px 0;
+    img {
+      width: 50%;
+      height: 50%;
+    }
+  }
+`
 
 
 export default function UserProfile() {
+  const { address, isConnected } = useAccount();
   return (
-    <div>
-        UserProfile
-    </div>
+    <UserProfileContainer>
+       <div className="header">
+          <img src='./my-profile-header.png'/>
+          <span>Wallet Address: {address ? formatAddress(address) : '--'}</span>
+        </div>
+    </UserProfileContainer>
   );
 }
