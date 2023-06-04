1c564162764bb60faa654ef12b997b591aa820d2
sanshi <sanshi@sanshideMacBook-Pro.local>
2023-06-04 22:53:11 +0800

    feat: update code

--- src/components/Footer/index.tsx
+++ src/components/Footer/index.tsx
@@ -2,7 +2,8 @@ import * as React from 'react';
 import styled from '@emotion/styled';
 
 const FooterContainer = styled.div`
-  width: 100%;
+  width: 100vw;
+  height: 120px;
   background: #142132;
   .bg {
     background: url('./footer-bg.png');
@@ -14,7 +15,7 @@ export default function Footer() {
   return (
     <FooterContainer>
        <div className='bg'>
-
+          fa
        </div>
     </FooterContainer>
   );
