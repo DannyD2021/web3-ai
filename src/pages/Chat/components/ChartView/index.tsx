d38bccffd084e6555eca77ede354a42af85eecad
sanshi <sanshi@sanshideMBP.lan>
2023-06-17 15:08:26 +0800

    feat: add chart area line

--- src/pages/Chat/components/ChartView/index.tsx
+++ src/pages/Chat/components/ChartView/index.tsx
@@ -1,7 +1,8 @@
 import React from "react";
 import styled from '@emotion/styled';
 import _ from "underscore";
-import { Line } from '@ant-design/plots';
+import { lineData, areaData } from "./mockData";
+import ReactECharts from 'echarts-for-react';
 
 const ChartViewContainer = styled.div`
     margin-top: 10px;
@@ -14,180 +15,99 @@ const ChartViewContainer = styled.div`
     }
 `
 
-
-const lineData = {
-    "chart_id": 288,
-    "type": "chart",
-    "title": "ChartCustom NFT Floor Tracker",
-    "render_type": "line",
-    "render_options": {
-        "sortX": true,
-        "xAxis": {
-            "type": "-",
-            "title": {
-                "text": ""
-            },
-            "tickFormat": ""
-        },
-        "yAxis": [
-            {
-                "type": "-",
-                "title": {
-                    "text": "Price (ETH)"
-                }
-            }
-        ],
-        "legend": {
-            "enabled": false
-        },
-        "series": {
-            "percentValues": false
-        },
-        "reverseX": false,
-        "numberFormat": "0.00",
-        "columnMapping": {
-            "Day": "x",
-            "Time": "x",
-            "Time Interval": "x",
-            "Floor (Approx)": "y",
-            "Floor Price Ξ": "y"
-        },
-        "seriesOptions": {
-            "Floor (Approx)": {
-                "type": "area",
-                "yAxis": 0,
-                "zIndex": 0
-            }
-        },
-        "valuesOptions": {},
-        "showDataLabels": false,
-        "globalSeriesType": "line"
-    },
-    "data": [
-        {
-            "Time Interval": "2021-06-30T00:00:00+00:00",
-            "Volume Ξ": 436.2504204183215898,
-            "Floor Price Ξ": 0.07
-        },
-        {
-            "Time Interval": "2021-07-01T00:00:00+00:00",
-            "Volume Ξ": 234.4223984531985889,
-            "Floor Price Ξ": 0.067
-        },
-        {
-            "Time Interval": "2021-07-02T00:00:00+00:00",
-            "Volume Ξ": 126.2050680386772336,
-            "Floor Price Ξ": 0.0553
-        },
-        {
-            "Time Interval": "2021-07-03T00:00:00+00:00",
-            "Volume Ξ": 41.3146717641353939,
-            "Floor Price Ξ": 0.053
-        },
-        {
-            "Time Interval": "2021-07-04T00:00:00+00:00",
-            "Volume Ξ": 40.3115495693165303,
-            "Floor Price Ξ": 0.049
-        },
-        {
-            "Time Interval": "2021-07-05T00:00:00+00:00",
-            "Volume Ξ": 70.1775439060451407,
-            "Floor Price Ξ": 0.04
-        },
-        {
-            "Time Interval": "2021-07-06T00:00:00+00:00",
-            "Volume Ξ": 22.0940635773107597,
-            "Floor Price Ξ": 0.0488
-        },
-        {
-            "Time Interval": "2021-07-07T00:00:00+00:00",
-            "Volume Ξ": 41.6046669740563740,
-            "Floor Price Ξ": 0.04991
-        },
-        {
-            "Time Interval": "2021-07-08T00:00:00+00:00",
-            "Volume Ξ": 15.4069055077018367,
-            "Floor Price Ξ": 0.0605
-        },
-        {
-            "Time Interval": "2021-07-09T00:00:00+00:00",
-            "Volume Ξ": 21.0877440616146936,
-            "Floor Price Ξ": 0.059
-        },
-        {
-            "Time Interval": "2021-07-10T00:00:00+00:00",
-            "Volume Ξ": 11.5725165231055101,
-            "Floor Price Ξ": 0.0641
-        },
-        {
-            "Time Interval": "2021-07-11T00:00:00+00:00",
-            "Volume Ξ": 12.6712165231055101,
-            "Floor Price Ξ": 0.062400000000000004
-        },
-        {
-            "Time Interval": "2021-07-12T00:00:00+00:00",
-            "Volume Ξ": 16.4999954129885269,
-            "Floor Price Ξ": 0.059
-        },
-        {
-            "Time Interval": "2021-07-13T00:00:00+00:00",
-            "Volume Ξ": 31.6202752745252696,
-            "Floor Price Ξ": 0.0698
-        },
-        {
-            "Time Interval": "2021-07-14T00:00:00+00:00",
-            "Volume Ξ": 18.8006165231055101,
-            "Floor Price Ξ": 0.065
-        },
-        {
-            "Time Interval": "2021-07-15T00:00:00+00:00",
-            "Volume Ξ": 22.3988407595830150,
-            "Floor Price Ξ": 0.06712
-        },
-        {
-            "Time Interval": "2021-07-16T00:00:00+00:00",
-            "Volume Ξ": 40.3112211694404074,
-            "Floor Price Ξ": 0.069
-        },
-        {
-            "Time Interval": "2021-07-17T00:00:00+00:00",
-            "Volume Ξ": 13.2056312517608578,
-            "Floor Price Ξ": 0.07289028620988725
-        },
-    ]
-}
-
 const ChartView = ({ chartData }: any) => {
-    const { data = [], title, render_options = {}, render_type } = chartData || lineData;
-    const { columnMapping } = render_options || {};
+    const { data = [], title, render_options = {}, render_type } = chartData || areaData;
+    const { columnMapping, seriesOptions, yAxis, legend } = render_options || {};
     const columns = Object.keys(data[0]);
     const renderChart = () => {
-        if (render_type === 'line') {
+        if (['area', 'line'].includes(render_type)) {
+            let seriesNames = Object.keys(seriesOptions);
             const columnMappingX = [];
             const columnMappingY = [];
-            for (let key in columnMapping){
+            for (let key in columnMapping) {
                 if (columnMapping[key] === 'x') {
                     columnMappingX.push(key);
                 }
                 if (columnMapping[key] === 'y') {
                     columnMappingY.push(key);
-                } 
+                }
             }
-            const xField = _.intersection(columns, columnMappingX)[0];
-            const yField = _.intersection(columns, columnMappingY)[0];
-            const config = {
-                data,
-                xField,
-                yField,
-                // seriesField: 'category',
-                // xAxis: {
-                //   type: 'time',
-                // },
-                // yAxis: {
-                // },
-              };
             
-            return <Line {...config}/>
+            const xField = _.intersection(columns, columnMappingX)[0];
+            // speical case, seriesOptions's key is not in columns
+            seriesNames = _.intersection(columns, seriesNames);
+            if (seriesNames.length === 0) {
+                seriesNames = _.intersection(columns, columnMappingY);
+            }
+            
+            const yAxisFormat = yAxis.map((axis: any, index: number) => {
+                return {
+                    type: 'value',
+                    name: axis.title.text,
+                    position: index % 2 === 0 ? 'left' : 'right',
+                    alignTicks: true,
+                    axisLine: {
+                        show: true,
+                    },
+                }
+            });
+            // handle data
+            const axisDatas = data.reduce((prev: any, item: any) => {
+                // xAxis data
+                if (!prev[xField]) {
+                    prev[xField] = [];
+                }
+                prev[xField].push(item[xField]);
+
+                // series data
+                seriesNames.forEach((seriesNames => {
+                    if (!prev[seriesNames]) {
+                        prev[seriesNames] = [];
+                    }
+                    prev[seriesNames].push(item[seriesNames]);
+                }))
+                return prev;
+            }, {})
+            // config series
+            const series: any = [];
+            seriesNames.forEach((seriesName) => {
+                const seriesConfig = seriesOptions[seriesName];
+                series.push({
+                    name: seriesName,
+                    type: 'line',
+                    areaStyle: seriesConfig?.type === 'area' ? {} : null,
+                    yAxisIndex: seriesConfig?.yAxis || 0,
+                    data: axisDatas[seriesName]
+                })
+            })
+            const option = {
+                tooltip: {
+                    trigger: 'axis',
+                    axisPointer: {
+                        type: 'cross'
+                    }
+                },
+                legend: {
+                    data: seriesNames,
+                    show: legend?.enabled,
+                    textStyle: {
+                        color: '#fff',
+                    },
+                },
+                xAxis: [
+                    {
+                        type: 'category',
+                        axisTick: {
+                            alignWithLabel: true
+                        },
+                        data: axisDatas[xField],
+                    }
+                ],
+                yAxis: yAxisFormat,
+                series,
+            };
+            console.log('option: ', option);
+            return <ReactECharts option={option} />
         }
         return ''
     }
