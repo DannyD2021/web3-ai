81c8878ac2616e536248ad7c27b841dc053f0cbb
sanshi <sanshi@sanshideMBP.lan>
2023-06-18 12:13:43 +0800

    feat: table paganation

--- src/pages/Chat/components/TableView/index.tsx
+++ src/pages/Chat/components/TableView/index.tsx
@@ -13,6 +13,12 @@ const TableViewContainer = styled.div`
     margin-top: 10px;
     background: rgba(55, 101, 239, 0.05);
 
+    h2 {
+        color: #8296d1;
+        margin: 10px;
+        font-size: 16px;
+    }
+
     th {
         color: #74acf6;
         font-weight: bolder;
@@ -25,15 +31,16 @@ const TableViewContainer = styled.div`
     th, td {
         padding: 12px;
         border-bottom: 1px solid rgb(94 120 200 / 50%);
+        font-size: 13px;
     }
 
 `
 
 const ROWS_PER_PAGE = 10;
 const TableView = ({ chartData }: any) => {
+    const {data = [], title } = chartData;
     const [page, setPage] = React.useState(0);
     const [rowsPerPage, setRowsPerPage] = React.useState(ROWS_PER_PAGE);
-    const data = chartData.data || [];
     if (data.length === 0) return null;
     const columnHeaders = Object.keys(data[0]);
 
@@ -53,6 +60,7 @@ const TableView = ({ chartData }: any) => {
 
     return (
         <TableContainer component={TableViewContainer}>
+            {title && <h2>{title}</h2>}
             <Table>
                 <TableHead>
                     <TableRow>
@@ -71,7 +79,6 @@ const TableView = ({ chartData }: any) => {
                 <TableFooter>
                     <TableRow>
                         <TablePagination
-                            align="left"
                             count={data.length}
                             rowsPerPage={rowsPerPage}
                             onRowsPerPageChange={handleChangeRowsPerPage}
